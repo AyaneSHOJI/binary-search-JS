a6f428367711caefc46e8567cd2a1ef451561d66
AyaneSHOJI <shojiayane@gmail.com>
2022-03-17 13:04:23 +0100

    loop pb solved

--- binarySearch.js
+++ binarySearch.js
@@ -1,10 +1,7 @@
-//array with rondam numbers in ascending order
-//find the number "67"
-let data = [16, 17, 23, 29, 31, 42, 45, 58, 62, 66, 67, 71, 78, 83, 88];
 
 //How to proceed ?
-//1. Get the middle number of the array
-//2. Compare the target number and the middle number
+//Get the middle number of the array
+//Compare the target number and the middle number
 //3. if ok, return
 //4. if not, compare the target number and the middle number
 //5. get the middle number of the half of array according to step 4 result
@@ -12,23 +9,30 @@ let data = [16, 17, 23, 29, 31, 42, 45, 58, 62, 66, 67, 71, 78, 83, 88];
 //7. Get the middle number of the half of array according to step 6 result
 
 function binarySearch(array, target){
-    let startIndex = 0;
-    let endIndex = array.length - 1;
+    var startIndex = 0;
+    var endIndex = array.length - 1;
 
     while(startIndex <= endIndex){
-        let middleIndex = Math.floor((startIndex + endIndex)/2); //in the first loop, middleIndex = 7
-        if(array[middleIndex] === target){//In the first loop, not equal, 45 != 67
+        var middleIndex = Math.floor((startIndex + endIndex)/2); 
+        var middleNumber = array[middleIndex];
+        
+        if(middleNumber === target){
             return middleIndex;
         }
-        else if(array[middleIndex] < target){//In the first loop, 45 < 67
-            startIndex = middleIndex + 1;//startIndex has a new value, 8, then restart loop
+        else if(middleNumber < target){
+            startIndex = middleIndex + 1;
         }
+        else if(middleNumber > target){
+            endIndex = middleIndex - 1;
+        } 
         else{
-            startIndex = middleIndex - 1;
+            return middleIndex;
         }
     }
-    //target not found
-    return -1;
 }
 
-Console.log(binarySearch(data, 67));
+//array with rondam numbers in ascending order
+//find the number "67"
+let data = [16, 17, 23, 29, 31, 42, 45, 58, 62, 66, 67, 71, 78, 83, 88];
+
+console.log("Target number index is at " + binarySearch([16, 17, 23, 29, 31, 42, 45, 58, 62, 66, 67, 71, 78, 83, 88], 17));
